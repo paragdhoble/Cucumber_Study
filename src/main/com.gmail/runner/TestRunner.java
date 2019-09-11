c87ccda6178cf699334b60b76e694528699444b0
PaRaG <paragd001@gmail.com>
2019-09-12 00:32:25 +0530

    Cucumber features parameters

--- src/main/com.gmail/runner/TestRunner.java
+++ src/main/com.gmail/runner/TestRunner.java
@@ -7,6 +7,11 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Study\\Automation\\src\\main\\com.gmail\\MyApplication.feature", glue={"StepDefination"})
+@CucumberOptions(features = "C:\\Study\\Automation\\src\\main\\com.gmail\\MyApplication.feature",
+                glue={"StepDefination"},
+                //format = {"pretty", },
+                monochrome =  true ,
+                dryRun = false
+)
 public class TestRunner {
 }
